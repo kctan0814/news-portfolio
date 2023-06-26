15b36cc9e1b816a2dd530f84833ff8085742d0f9
Kc Tan <kctan@Kcs-MacBook-Air.local>
2023-06-26 15:05:43 +0100

    added extra code to make error handlers actually work

--- app/app.js
+++ app/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const { getTopics } = require('./controller/topics.controller');
+const { handlePsqlErrors, handleCustomErrors, handleServerErrors } = require('./errors/errors');
 
 const app = express();
 
 app.get('/api/topics', getTopics)
+
+app.use(handleCustomErrors)
+
+app.use(handlePsqlErrors)
+
+app.use(handleServerErrors)
 
 module.exports = app;
 
--- app/controller/topics.controller.js
+++ app/controller/topics.controller.js
@@ -1,7 +1,9 @@
 const { selectTopics } = require("../model/topics.model")
 
-exports.getTopics = (_, res) => {
-  selectTopics().then((topics) =>
+exports.getTopics = (_, res, next) => {
+  selectTopics()
+  .then((topics) =>
     res.status(200).send({topics})  
   )
+  .catch(next)
 }
