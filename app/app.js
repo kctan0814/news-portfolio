84582f4197750e16cdd2fa5006beac110c85c660
Kc Tan <kctan@Kcs-MacBook-Air.local>
2023-06-27 20:16:43 +0100

    Cleaned up console log and unnecessary code

--- app/app.js
+++ app/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const { getTopics } = require('./controller/topics.controller');
-const { handlePsqlErrors, handleCustomErrors, handleServerErrors } = require('./errors/errors');
+const { handleCustomErrors, handleServerErrors } = require('./errors/errors');
 
 const app = express();
 
 app.get('/api/topics', getTopics)
 
 app.use(handleCustomErrors)
-
-app.use(handlePsqlErrors)
 
 app.use(handleServerErrors)
 
