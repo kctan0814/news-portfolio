2233d9cf37ef0b540f2ca2d7b6b0ed162ea1150d
Kc Tan <kctan@Kcs-MacBook-Air.local>
2023-06-27 21:08:10 +0100

    finished controller and model code to make GET /api/articles function as intended

--- app/__tests__/app.test.js
+++ app/__tests__/app.test.js
@@ -30,22 +30,22 @@ describe('GET /api/articles', () => {
   test('200: returns a list of all articles', () => {
     return request(app)
       .get('/api/articles')
-      // .expect(200)
+      .expect(200)
       .then(({body: {articles}}) => {
-        // expect(articles.length).toBe()
-        // expect(Array.isArray(articles)).toBe(true)
-        // articles.forEach(article => {
-        //   expect(article).toMatchObject({
-        //     article_id: expect.ant(Number),
-        //     author: expect.any(String),
-        //     title: expect.any(String),
-        //     topic: expect.any(String),
-        //     created_at: expect.any(String),
-        //     article_img_url: expect.any(String),
-        //     comment_count: expect.ant(Number),
-        //     votes: expect.ant(Number)
-        //   })
-        // })
+        expect(articles.length).toBe(5)
+        expect(Array.isArray(articles)).toBe(true)
+        articles.forEach(article => {
+          expect(article).toMatchObject({
+            article_id: expect.any(Number),
+            author: expect.any(String),
+            title: expect.any(String),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            article_img_url: expect.any(String),
+            comment_count: expect.any(String),
+            votes: expect.any(Number)
+          })
+        })
       })
   })
 })
--- app/model/articles.model.js
+++ app/model/articles.model.js
@@ -1,3 +1,21 @@
+const db = require('../../db/connection')
+
 exports.selectArticles = () => {
-  
+  return db.query(`
+  SELECT 
+  a.article_id, 
+  a.author, 
+  a.title, 
+  a.topic, 
+  a.created_at, 
+  a.article_img_url,
+  a.votes,
+  COUNT(c.comment_id) as comment_count
+  FROM articles a 
+  JOIN comments c
+  ON a.article_id = c.article_id
+  GROUP BY a.article_id`)
+  .then(({rows}) => {
+    return rows;
+  })
 }
