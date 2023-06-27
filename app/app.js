fc6dc50728a4b57701a50c378058bfea104fe09b
Kc Tan <kctan@Kcs-MacBook-Air.local>
2023-06-27 14:44:14 +0100

    Created endpoints.json and api.controller.js. Updated test to test for GET /api endpoint

--- app/app.js
+++ app/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { getTopics } = require('./controller/topics.controller');
 const { handlePsqlErrors, handleCustomErrors, handleServerErrors } = require('./errors/errors');
+const { getEndpoints } = require('./controller/api.controller');
 
 const app = express();
 
 app.get('/api/topics', getTopics)
 
-app.use(handleCustomErrors)
+app.get('/api', getEndpoints)
 
-app.use(handlePsqlErrors)
+app.use(handleCustomErrors)
 
 app.use(handleServerErrors)
 
