d7200cca947f285011018fc0189ef329ab3b8fbb
Kc Tan <kctan@Kcs-MacBook-Air.local>
2023-06-29 10:31:47 +0100

    Revert "Cleaned up console log and unnecessary code"
    This reverts commit 84582f4197750e16cdd2fa5006beac110c85c660.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { getTopics } = require('./controller/topics.controller');
-const { handleCustomErrors, handleServerErrors } = require('./errors/errors');
+const { handlePsqlErrors, handleCustomErrors, handleServerErrors } = require('./errors/errors');
 
 const app = express();
 
 app.get('/api/topics', getTopics)
 
 app.use(handleCustomErrors)
+
+app.use(handlePsqlErrors)
 
 app.use(handleServerErrors)
 
