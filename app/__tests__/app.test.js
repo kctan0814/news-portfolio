c2ee5c966719776ca927a0e1232c1cc6153122ab
Kc Tan <kctan@Kcs-MacBook-Air.local>
2023-07-16 15:17:16 +0100

    only commiting to check other branches

--- app/__tests__/app.test.js
+++ app/__tests__/app.test.js
@@ -149,7 +149,7 @@ describe('GET /api/articles', () => {
 })
 
 describe('POST /api/articles', () => {
-  test('200: inserts body into the comments table in the database and returns the insered data', () => {
+  test('201: inserts body into the comments table in the database and returns the insered data', () => {
     const bodyToSend = {
       username: 'lurker',
       body: 'This is my first time commenting here ever!! omg!!!'
@@ -167,6 +167,41 @@ describe('POST /api/articles', () => {
           author: bodyToSend.username,
           body: bodyToSend.body
         })
+      })
+  })
+  test('201: still inserts the data with unnecessary properties', () => {
+    const bodyToSend = {
+      username: 'lurker',
+      body: 'This is my first time commenting here ever!! omg!!!',
+      planets_in_solar_system: 8,
+      entropy: 'Whats that?'
+    }
+    return request(app)
+      .post('/api/articles/5/comments')
+      .send(bodyToSend)
+      .expect(200)
+      .then(({body: {comment}}) => {
+        expect(comment).toMatchObject({
+          comment_id: expect.any(Number),
+          article_id: 5,
+          votes: 0,
+          created_at: expect.any(String),
+          author: bodyToSend.username,
+          body: bodyToSend.body
+        })
+      })
+  })
+  test.skip('400: returns an error message of "Not found" when the username passed is not in the database', () => {
+    const bodyToSend = {
+      username: 'kctan36',
+      body: 'What the point in living? (this is a joke)'
+    }
+    return request(app)
+      .post('/api/articles/1/comments')
+      .send(bodyToSend)
+      .expect(404)
+      .then(({body: {msg}}) => {
+        expect(msg).toBe('Not found')
       })
   })
   test('400: returns an error mesagge of "Bad request" when id passed is not a number', () => {
